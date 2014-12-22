ada1d66895d10757258776122de85f96e42c5690
Alex Turbov <i.zaufi@gmail.com>
2014-12-22 15:52:16 +0300

    fix getting svn branch for modern subversion

--- context-checkers.d/52svn.sh
+++ context-checkers.d/52svn.sh
@@ -18,7 +18,7 @@ function _52_is_svn_repo()
 function _get_svn_branch()
 {
     local _gsb__output_var=$1
-    local _gsb__url=`svn info | grep URL | sed 's,URL:\s*,,'`
+    local _gsb__url=`svn info | grep '^URL' | sed 's,URL:\s*,,'`
     local _gsb__branch=`sed -e 's,.*/branches/\([^/]\+\).*,\1,' -e 't end' -e 'd' -e ':end' <<<${_gsb__url}`
     if [ -z "${_gsb__branch}" ]; then
         _gsb__branch=`sed -e 's,.*/\(trunk\).*,\1,' -e 't end' -e 'd' -e ':end' <<<${_gsb__url}`
