5a74a1a8f1e3639e1625e1a390e2c888f5491b94
Alex Turbov <i.zaufi@gmail.com>
2016-11-04 00:47:58 +0800

    consider `/etc/debian_chroot` as indicator of running chroot

--- context-checkers.d/00schroot.sh
+++ context-checkers.d/00schroot.sh
@@ -12,12 +12,16 @@
 
 function _00_is_under_schroot()
 {
-    return `test -n "${SCHROOT_CHROOT_NAME}"`
+    return `test -n "${SCHROOT_CHROOT_NAME}" -o -f /etc/debian_chroot`
 }
 
 function _show_schroot()
 {
-    printf "${sp_warn}${SCHROOT_CHROOT_NAME}"
+    if [ -f /etc/debian_chroot ]; then
+        printf "${sp_warn}`cat /etc/debian_chroot`"
+    else
+        printf "${sp_warn}${SCHROOT_CHROOT_NAME}"
+    fi
 }
 
 SMART_PROMPT_PLUGINS[_00_is_under_schroot]=_show_schroot
