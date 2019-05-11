a2802935eb1be925b0cb3185e335b62a1d0775eb
Alex Turbov <i.zaufi@gmail.com>
2019-05-11 22:51:58 +0800

    Add a segment w/ a last command exit status

---
+++ context-checkers.d/99prev-cmd-exit-code.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+# The most "right" segment (to attract user's attention)
+# w/ the exit code of the previous command.
+# NOTE Do not show enything for "success" exit code.
+#
+# Copyright (c) 2019 Alex Turbov <i.zaufi@gmail.com>
+#
+# This file is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+
+function _99_check_prev_cmd_exit_code()
+{
+    return $([[ ${_sp__prev_cmd_exit_code} != 0 ]])
+}
+
+function _show_prev_cmd_exit_code()
+{
+    printf "${sp_color_alert}${_sp__prev_cmd_exit_code}"
+}
+
+SMART_PROMPT_PLUGINS[_99_check_prev_cmd_exit_code]=_show_prev_cmd_exit_code
