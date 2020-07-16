b2dddb0b6b005861a9c163c1cfd6e4e374e41949
Alex Turbov <i.zaufi@gmail.com>
2020-07-16 14:42:48 +0800

    Fix: Drop redundant segment for `/etc/paludis`

--- context-checkers.d/70paludis.sh
+++ context-checkers.d/70paludis.sh
@@ -23,7 +23,7 @@ function _show_paludis_info()
     if _find_program cave _cave_bin; then
         local _spi__repos_color
         _get_color_param SP_PALUDIS_REPOS_COLOR sp_color_misc _spi__repos_color
-        printf "${sp_seg}${_spi__repos_color}%d reps" $(${_cave_bin} print-repositories | wc -l)
+        printf "${_spi__repos_color}%d reps" $(${_cave_bin} print-repositories | wc -l)
     fi
 }
 SMART_PROMPT_PLUGINS[_70_is_inside_of_paludis_sysconf_dir]=_show_paludis_info
