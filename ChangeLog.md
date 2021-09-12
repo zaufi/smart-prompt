752a4a78ea747fa46f847e9bf7a673e9af89d29b
Alex Turbov <i.zaufi@gmail.com>
2021-09-12 20:11:30 +0300

    Feature: Add `SP_MARKS_MAP` and `SP_MARK_PATTERNS_MAP`

--- ChangeLog.md
+++ ChangeLog.md
@@ -12,21 +12,21 @@ and this project adheres to [Semantic Versioning](http://semver.org/).
 ### Added
 
 - Add options `USE_GIT` and `USE_SVN` with default value `AUTO` to enforce
-  install corresponding checker, even if no program has found;
-- Add option `USE_GENTOO` with default value `AUTO` to install Gentoo
-  specific checkers;
+  install corresponding checker, even if no program has found.
 - Colorize CMake build types. One can set `SP_CMAKE_<UPCASE_NAME>_BUILD_TYPE_COLOR`
-  to desired color. If not set, default `SP_CMAKE_BUILD_TYPE_COLOR` will be used;
+  to desired color. If not set, default `SP_CMAKE_BUILD_TYPE_COLOR` will be used.
 - Detect JS project dir and extract package name and version from `package.json`.
+- Add `SP_MARKS_MAP` and `SP_MARK_PATTERNS_MAP` to append _indicators_ to the path
+  segment based on files (patterns) present in the current directory.
 
 ### Changed
 
 - Review code and use `[[` more extensively. Also replace backticks with `$()`.
-  Speed up a little: avoid some calls to external programs;
-- CMake options changed from `USE_xxx` to `WITH_xxx`;
-- Use unicode symbols for VCS branch and Python virtual env;
-- Use XDG specification to get the user configs;
+  Speed up a little: avoid some calls to external programs.
+- CMake options changed from `USE_xxx` to `WITH_xxx`.
+- Use unicode symbols for VCS branch and Python virtual env.
+- Use XDG specification to get the user configs.
 - CLI binding to search for running process learned to use pipe instead of
   shell variables and can highlight found substring.
 
-[Unreleased]: https://github.com/zaufi/smart-prompt/compare/version-1.4.0...HEAD
+[Unreleased]: https://github.com/zaufi/smart-prompt/compare/ddefc56...HEAD
--- context-checkers.d/02pwd.sh
+++ context-checkers.d/02pwd.sh
@@ -2,7 +2,7 @@
 #
 # Append current path segment
 #
-# Copyright (c) 2014-2018 Alex Turbov <i.zaufi@gmail.com>
+# Copyright (c) 2014-2021 Alex Turbov <i.zaufi@gmail.com>
 #
 # This file is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -25,27 +25,29 @@ function _show_pwd()
     local _sp__pwd_color
     _get_color_param SP_PWD_COLOR sp_color_info _sp__pwd_color
 
-    local _sp__pwd_empty_dir_mark
+    local _sp__pwd_marks
     if [[ -z $(shopt -s nullglob; echo *) ]]; then
-        _get_color_param SP_EMPTY_DIR_COLOR sp_color_debug _sp__pwd_color
-        _sp__pwd_empty_dir_mark=${SP_EMPTY_DIR_MARK}
+        _get_color_param SP_EMPTY_DIR_COLOR sp_color_debug _sp__pwd_empty_dir_color
+        _sp__pwd_marks=${_sp__pwd_empty_dir_color}${SP_EMPTY_DIR_MARK}
     fi
 
-    local _sp__pwd_buildable_mark
-    _get_color_param SP_PWD_COLOR sp_color_info _sp__pwd_color
-    if [[ -f Makefile ]]; then
-        local _sp__pwd_mk_color
-        _get_color_param SP_MAKEFILE_MARK_COLOR sp_color_info _sp__pwd_mk_color
-        _sp__pwd_buildable_mark="${_sp__pwd_mk_color}${SP_MAKEFILE_MARK:-❲🚧❳}"
+    local _sp__pwd_pair
+    local _sp__pwd_key
+    local _sp__pwd_glob
+    for _sp__pwd_pair in "${SP_MARKS_MAP[@]}"; do
+        IFS=': ' read -r _sp__pwd_key _sp__pwd_glob <<<${_sp__pwd_pair}
+        _sp__pwd_marks+=$([[ -e ${_sp__pwd_glob} ]] && echo ${_sp__pwd_key})
+    done
+    for _sp__pwd_pair in "${SP_MARK_PATTERNS_MAP[@]}"; do
+        IFS=':' read -r _sp__pwd_key _sp__pwd_glob <<<${_sp__pwd_pair}
+        _sp__pwd_marks+=$([[ -n $(shopt -s extglob globstar nullglob; echo ${_sp__pwd_glob}) ]] && echo ${_sp__pwd_key})
+    done
+
+    if [[ -n ${_sp__pwd_marks} ]]; then
+        _sp__pwd_marks=${SP_OPEN_MARK:-❲}${_sp__pwd_marks}${SP_CLOSE_MARKS:-❳}
     fi
 
-    if [[ -f build.ninja ]]; then
-        local _sp__pwd_nj_color
-        _get_color_param SP_NINJA_MARK_COLOR sp_color_info _sp__pwd_nj_color
-        _sp__pwd_buildable_mark="${_sp__pwd_nj_color}${SP_NINJA_MARK:-❲🥷❳}"
-    fi
-
-    printf "${_sp__pwd_color}${_sp__dir_stack_size}\\w${_sp__pwd_empty_dir_mark}${_sp__pwd_buildable_mark}"
+    printf "${_sp__pwd_color}${_sp__dir_stack_size}\\w${_sp__pwd_marks}"
 }
 
 SMART_PROMPT_PLUGINS[_02_show_pwd]=_show_pwd
