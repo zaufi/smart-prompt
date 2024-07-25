ce22f198244db4195a5811abdb877a079e9a28dd
Alex Turbov <i.zaufi@gmail.com>
2024-07-25 16:24:54 +0400

    impr: split `00debian_chroot` into schroot and docker parts
    And conditionally installs schroot support.

---
+++ context-checkers.d/00debian_chroot_d.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# SPDX-FileCopyrightText: 2014 - 2024 Alex Turbov <i.zaufi@gmail.com>
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+#
+# Try to display a docker name
+#
+# NOTE Docker images expected to have their names in the `/etc/debian_chroot` file.
+#
+function _00_is_under_docker()
+{
+    [[ -e /.dockerenv && -e /etc/debian_chroot ]]
+}
+
+function _show_docker_debian_chroot()
+{
+    _get_color_param SP_CHROOT_NAME sp_color_warn _sdc__chroot_name
+    local _mark="${SP_DOCKER_MARK:-🐳:}"
+    if [[ ${SP_DOCKER_MARK} == 'none' ]]; then
+        _mark=""
+    fi
+    printf '%s%s%s' "${_sdc__chroot_name}" "${_mark}" "$(< /etc/debian_chroot)"
+}
+SMART_PROMPT_PLUGINS[_00_is_under_docker]=_show_docker_debian_chroot
--- context-checkers.d/00debian_chroot_s.sh
+++ context-checkers.d/00debian_chroot_s.sh
@@ -27,24 +27,3 @@ function _show_debian_chroot()
     fi
 }
 SMART_PROMPT_PLUGINS[_00_is_under_schroot]=_show_debian_chroot
-
-#
-# Try to display a docker name
-#
-# NOTE Docker images expected to have their names in the `/etc/debian_chroot` file.
-#
-function _00_is_under_docker()
-{
-    [[ -e /.dockerenv && -e /etc/debian_chroot ]]
-}
-
-function _show_docker_debian_chroot()
-{
-    _get_color_param SP_CHROOT_NAME sp_color_warn _sdc__chroot_name
-    local _mark="${SP_DOCKER_MARK:-🐳:}"
-    if [[ ${SP_DOCKER_MARK} == 'none' ]]; then
-        _mark=""
-    fi
-    printf '%s%s%s' "${_sdc__chroot_name}" "${_mark}" "$(< /etc/debian_chroot)"
-}
-SMART_PROMPT_PLUGINS[_00_is_under_docker]=_show_docker_debian_chroot
